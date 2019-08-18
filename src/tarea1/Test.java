300d72a43b67beaa40d23688a4bd60869425c42c
Vito217 <vito.segurac@hotmail.com>
2019-08-18 15:51:30 -0400

    Matriz de confusion

--- src/tarea1/Test.java
+++ src/tarea1/Test.java
@@ -1,12 +1,12 @@
 package tarea1;
 
-import java.awt.dnd.DropTarget;
-import java.util.ArrayList;
-import java.util.Collections;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Arrays;
 import java.util.Random;
+import java.util.Scanner;
 import javax.swing.SwingUtilities;
 import javax.swing.WindowConstants;
-import javax.xml.crypto.Data;
 
 public class Test {
 
@@ -28,16 +28,67 @@ public class Test {
         return new DataTuple(dataset, output);
     }
 
+    public static DataTuple read_iris_dataset(String file){
+
+        double[][] data = new double[150][4];
+        double[][] target = new double[150][3];
+
+        try {
+
+            Scanner f = new Scanner(new File(file));
+            int row = 0;
+
+            // Getting data
+            while(f.hasNextLine()){
+                String l = f.nextLine();
+                String[] d = l.split(",");
+                for(int col=0; col<d.length; col++){
+                    try{
+                        data[row][col] = Double.parseDouble(d[col]);
+                    }
+                    catch (NumberFormatException e){
+                        switch (d[col]) {
+                            case "Iris-setosa":
+                                target[row] = new double[]{1.0, 0.0, 0.0};
+                                break;
+                            case "Iris-versicolor":
+                                target[row] = new double[]{0.0, 1.0, 0.0};
+                                break;
+                            case "Iris-virginica":
+                                target[row] = new double[]{0.0, 0.0, 1.0};
+                                break;
+                        }
+                    }
+                }
+                row++;
+            }
+
+            f.close();
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        return new DataTuple(data, target);
+    }
+
+    public static double[][] normalize_iris_data(double[][] data){
+
+        for(int i=0; i<data.length; i++){
+            for(int j=0; j<data[i].length; j++){
+                data[i][j] = (((data[i][j]-0.0)*(1.0-0.0))/(9.9-0.0)) + 0.0;
+            }
+        }
+
+        return data;
+    }
+
     public static void main(String[] args){
 
+        /**
         DataTuple train_data = generate_random_binary_dataset(1000, 1,0);
 
         DataTuple eval_data = generate_random_binary_dataset(500, 1,0);
-
-        //Perceptron p = new Perceptron(2, 0.1, "binary", "cross");
-
-        //p.train(10, train_data.getInputs(), train_data.getOutput());
-        //DataTuple dt = p.evaluate(eval_data.getInputs(), eval_data.getOutput());
 
         NeuralNetwork net = new NeuralNetwork(
                 new Layer[]{
@@ -59,8 +110,86 @@ public class Test {
             example.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
             example.setVisible(true);
         });
+        **/
 
-        //System.out.println("El test paso correctamente");
+        DataTuple iris_data_tuple =
+                read_iris_dataset("C:\\Users\\VictorStefano\\IdeaProjects\\CC5114\\src\\tarea1\\iris.data");
 
+        double[][] data = normalize_iris_data(iris_data_tuple.getInputs());
+        double[][] target = iris_data_tuple.getOutput();
+
+        double[][] train_data = new double[90][4];
+        double[][] train_target = new double[90][3];
+
+        double[][] eval_data = new double[60][4];
+        double[][] eval_target = new double[60][3];
+
+        for(int i=0; i<90; i++){
+            train_data[i] = data[i];
+            train_target[i] = target[i];
+        }
+
+        for(int i=0; i<60; i++){
+            eval_data[i] = data[i+90];
+            eval_target[i] = target[i+90];
+        }
+
+        NeuralNetwork n = new NeuralNetwork(
+                new Layer[]{
+                        new Layer(
+                                3,
+                                4,
+                                0.1,
+                                "sigmoid",
+                                "cross"
+                        )
+                }
+        );
+
+        n.train(train_data, train_target, 10000);
+        DataTuple dt = n.eval(eval_data, eval_target);
+        double[][] eval_output = dt.getOutput();
+
+        // Confusion matrix
+        double[][] conf_matrix = new double[3][3];
+        for(int i=0; i<3; i++){
+            for(int j=0; j<20 && i==0; j++){
+                if(Math.max(eval_output[j][0], Math.max(eval_output[j][1], eval_output[j][2]))==eval_output[j][0]){
+                    conf_matrix[i][0] += 1.0/20.0;
+                }
+                else if(Math.max(eval_output[j][0], Math.max(eval_output[j][1], eval_output[j][2]))==eval_output[j][1]){
+                    conf_matrix[i][1] += 1.0/20.0;
+                }
+                else{
+                    conf_matrix[i][2] += 1.0/20.0;
+                }
+            }
+            for(int j=20; j<40 && i==1; j++){
+                if(Math.max(eval_output[j][0], Math.max(eval_output[j][1], eval_output[j][2]))==eval_output[j][0]){
+                    conf_matrix[i][0] += 1.0/20.0;
+                }
+                else if(Math.max(eval_output[j][0], Math.max(eval_output[j][1], eval_output[j][2]))==eval_output[j][1]){
+                    conf_matrix[i][1] += 1.0/20.0;
+                }
+                else{
+                    conf_matrix[i][2] += 1.0/20.0;
+                }
+            }
+            for(int j=40; j<60 && i==2; j++){
+                if(Math.max(eval_output[j][0], Math.max(eval_output[j][1], eval_output[j][2]))==eval_output[j][0]){
+                    conf_matrix[i][0] += 1.0/20.0;
+                }
+                else if(Math.max(eval_output[j][0], Math.max(eval_output[j][1], eval_output[j][2]))==eval_output[j][1]){
+                    conf_matrix[i][1] += 1.0/20.0;
+                }
+                else{
+                    conf_matrix[i][2] += 1.0/20.0;
+                }
+            }
+        }
+
+        for(double[] row: conf_matrix){
+            System.out.println(Arrays.toString(row));
+        }
     }
 }
